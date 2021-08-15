65bc186cdfa33ae5de3442b537c6117d0959a2fe
Vishnu Anand <vishnuanand2000@gmail.com>
2021-08-15 13:40:52 +0530

    Created New Activity for OCR functionality

---
+++ app/src/main/java/com/example/note_inghill/ImageToTextActivity.java
@@ -0,0 +1,14 @@
+package com.example.note_inghill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageToTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_to_text);
+    }
+}
