993f55ecdb0b73753399b97c7037f39897b91e10
Vishnu Anand <vishnuanand2000@gmail.com>
2021-08-15 13:43:18 +0530

    Added information about the activity as comments

--- app/src/main/java/com/example/note_inghill/ImageToTextActivity.java
+++ app/src/main/java/com/example/note_inghill/ImageToTextActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class ImageToTextActivity extends AppCompatActivity {
 
+    // This Activity is responsible for handling Image to Text.
+    // The screen will contain a camera view and an upload button.
+        // - The Upload button will run amplify function to connect and upload image to S3
+        // - On upload to S3 bucket, a lambda function is triggered which then performs OCR.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_to_text);
+
+        
+
     }
 }
