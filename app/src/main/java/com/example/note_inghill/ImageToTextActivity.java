870a3c240ba5a168929d3934877bd30a440eb0e4
Vishnu Anand <vishnuanand2000@gmail.com>
2021-08-23 15:10:31 +0530

    Cleaned Code and Added Comments

--- app/src/main/java/com/example/note_inghill/ImageToTextActivity.java
+++ app/src/main/java/com/example/note_inghill/ImageToTextActivity.java
@@ -101,6 +101,7 @@ public class ImageToTextActivity extends AppCompatActivity {
                 .start(this);
     }
 
+    // On Activity Result
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
