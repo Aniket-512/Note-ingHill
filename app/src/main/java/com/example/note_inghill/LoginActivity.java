b1ab7e40a6ac880d05ef0e5f4ba6d577fc9f32fe
Chinmaya Ravi <chinmayaravi@gmail.com>
2021-08-24 21:57:57 +0530

    Text summarisation button added

---
+++ app/src/main/java/com/example/note_inghill/LoginActivity.java
@@ -0,0 +1,106 @@
+package com.example.note_inghill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // NOTE: Additional functionality+UI remaining - Forgot password
+
+    private Button loginButton;
+    private Button registerButton;
+    private TextView userEmail; // Mobile number UI field variable
+    private TextView userPassword; // Password UI field variable
+
+    // Maximum number of login attempts
+    private int COUNTER;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        // Intent to shift to Main Activity upon logging in successfully
+        Intent mainIntent = new Intent(this, MainActivity.class);
+
+        // Set max number of login attempts
+        COUNTER=3;
+
+        // Amplify Auth plugin setup on device
+        try {
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i("Amplify", "Initialized Amplify plugins");
+        } catch (AmplifyException error) {
+            Log.e("Amplify", "Could not initialize Amplify plugins", error);
+        }
+
+        // Intent to shift to Register Activity in case of new user
+        Intent registerIntent = new Intent(this, RegisterActivity.class);
+
+        // Check whether user is signed in or not
+        // If signed in -> show Main Activity
+        Amplify.Auth.fetchAuthSession(
+                result -> {Log.i("AmplifyAuthSession", result.toString());
+                if(result.isSignedIn())
+                    startActivity(mainIntent);},
+                error -> Log.e("AmplifyAuthSession", error.toString())
+        );
+
+        // Listen for click on Register button, if clicked move to -> Register Activity
+        registerButton = findViewById(R.id.registerActivity_button);
+        registerButton.setOnClickListener(v -> startActivity(registerIntent));
+
+        // Connect Java code variables to corresponding UI elements
+        userEmail = findViewById(R.id.email_login);
+        userPassword = findViewById(R.id.password_login);   // Test password -
+
+        // Login button click -> sign in attempt -> show Main activity
+        loginButton = findViewById(R.id.loginActivity_button);
+        loginButton.setOnClickListener(v -> attemptLogin());
+    }
+
+    // Function to try logging in user
+    private void attemptLogin(){
+        // Check if fields are empty, toast accordingly
+        if(TextUtils.isEmpty(userEmail.getText()))
+            Toast.makeText(this,"Please enter your Email ID",Toast.LENGTH_LONG).show();
+        else if(TextUtils.isEmpty(userPassword.getText()))
+            Toast.makeText(this, "Please enter your Password", Toast.LENGTH_LONG).show();
+
+        // Test account credentials -
+        // Email - test@example.com
+        // Password - Test123@#
+
+        // If input fields are not empty, and max login attempts have not been made -> try to login user
+        else if(!(userPassword.getText().toString().isEmpty() && userEmail.getText().toString().isEmpty()) && COUNTER !=0){
+            // Intent to shift to Main Activity upon logging in successfully
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            // Attempt to sign in
+            // Note: Username passed MUST be Email ID; name won't work.
+            Amplify.Auth.signIn(
+                    userEmail.getText().toString(), userPassword.getText().toString(),
+                    // Log result of sign in attempt, if successful move to MainActivity
+                    result -> {Log.i("AmplifyAuthLogin", result.isSignInComplete() ? "Sign in  successful":"Sign in FAILED");
+                        if(result.isSignInComplete())
+                            startActivity(mainIntent);},
+                    error -> Log.e("AmplifyAuthLogin", error.toString())
+            );
+            // Decrease number of tries
+            COUNTER -=1;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/note_inghill/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.note_inghill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.amplifyframework.auth.AuthChannelEventName;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.core.InitializationStatus;
+import com.amplifyframework.hub.HubChannel;
+
+// Main Activity only has a sign out button for now
+// Will show Lecture recording and Image-to-Text feature on this screen in future
+public class MainActivity extends AppCompatActivity {
+//Password : Ronaldoidol#7
+
+    private Button signoutButton;
+    private Button imageToText; // Button press loads new activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Unused, implementation remaining
+        userStatus();
+        // Sign out intent - move back to AuthActivity
+        Intent signoutIntent = new Intent(this, LoginActivity.class);
+
+        //Image to Text Button
+        imageToText = findViewById(R.id.imageToTextId);
+        imageToText.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ImageToTextActivity.class);
+            startActivity(intent);
+        });
+
+        // Sign out using Amplify Auth when button clicked
+        signoutButton = findViewById(R.id.signout_button);
+        signoutButton.setOnClickListener(v -> {
+            Amplify.Auth.signOut(
+                    () -> Log.i("AmplifyAuth", "Signed out successfully"),
+                    error -> Log.e("AmplifyAuth", error.toString())
+            );
+            // start AuthActivity (login)
+            startActivity(signoutIntent);
+        });
+    }
+
+    // Unused function, implementation remaining to return user auth status
+    private void userStatus(){
+        // AWS Cognito Auth Plugin sends important Auth events through Amplify Hub.
+        Amplify.Hub.subscribe(HubChannel.AUTH,
+                hubEvent -> {
+                    if (hubEvent.getName().equals(InitializationStatus.SUCCEEDED.toString())) {
+                        Log.i("AmplifyAuthHub", "Auth Hub successfully initialized");
+                    } else if (hubEvent.getName().equals(InitializationStatus.FAILED.toString())){
+                        Log.i("AmplifyAuthHub", "Auth Hub failed to succeed");
+                    } else {
+                        switch (AuthChannelEventName.valueOf(hubEvent.getName())) {
+                            case SIGNED_IN:
+                                Log.i("AmplifyAuthHub", "Auth just became signed in.");
+                                break;
+                            case SIGNED_OUT:
+                                Log.i("AmplifyAuthHub", "Auth just became signed out.");
+                                break;
+                            case SESSION_EXPIRED:
+                                Log.i("AmplifyAuthHub", "Auth session just expired.");
+                                break;
+                            default:
+                                Log.w("AmplifyAuthHub", "Unhandled Auth Event: " + AuthChannelEventName.valueOf(hubEvent.getName()));
+                                break;
+                        }
+                    }
+                }
+        );
+    }
+}
