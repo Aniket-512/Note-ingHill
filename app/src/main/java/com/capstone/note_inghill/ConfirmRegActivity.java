4b411ae46f6e38fb330af8becf55b7e5b27d115f
Vishnu Anand <vishnuanand2000@gmail.com>
2021-11-30 20:05:02 +0530

    Changed Package name from com.example to com.capstone

---
+++ app/src/main/java/com/capstone/note_inghill/ConfirmRegActivity.java
@@ -0,0 +1,43 @@
+package com.capstone.note_inghill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.amplifyframework.core.Amplify;
+import com.example.note_inghill.R;
+
+// Confirm User Registration with OTP
+public class ConfirmRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_reg);
+
+        // Get user's Email ID from RegisterActivity
+        String email = getIntent().getStringExtra("email");
+
+        // Connect UI elements to Java code - OTP text field, Confirm button
+        TextView otpField = findViewById(R.id.otp_field);
+        Button confirmOtpButton = findViewById(R.id.confirm_otp_button);
+
+        Intent mainIntent = new Intent(this, MainActivity.class);
+
+        confirmOtpButton.setOnClickListener(v -> {
+            // Ensure OTP field is not empty
+            assert !otpField.getText().toString().isEmpty();
+            // No error handling yet
+            Amplify.Auth.confirmSignUp(email, otpField.getText().toString(),
+                result -> {Log.i("AmplifyAuthConfirm", result.isSignUpComplete() ? "Confirm signUp succeeded" : "Confirm sign up not complete");
+                    if(result.isSignUpComplete())
+                        startActivity(mainIntent);},
+                error ->Log.e("AmplifyAuthConfirm", error.toString())
+            );
+        });
+    }
+}
---
+++ app/src/main/java/com/capstone/note_inghill/ImageToTextActivity.java
@@ -0,0 +1,256 @@
+package com.capstone.note_inghill;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.amplifyframework.core.Amplify;
+import com.example.note_inghill.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+
+public class ImageToTextActivity extends AppCompatActivity {
+
+    // This Activity is responsible for handling Image to Text.
+    // The screen will contain a camera view and an upload button.
+        // - Camera needs a cropper to crop the image into a square image.
+        // - The Upload button will run amplify function to connect and upload image to S3
+        // - On upload to S3 bucket, a lambda function is triggered which then performs OCR.
+
+    // Additional Feature would be to provide a gallery option as well.
+
+
+    private ImageView imageView;
+
+    //Global Variables
+    String imageUri = "";
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_to_text);
+
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+        
+        // Button Declarations
+        // XML Asset Declarations
+        Button openGallery = findViewById(R.id.Gallery_open_button);
+        Button uploadButton = findViewById(R.id.Upload_image_button);
+        Button summarizeButton = findViewById(R.id.Summarize);
+
+        // Image View
+        imageView = findViewById(R.id.imageView2);
+
+        // Open Gallery : Opens a image picker which is native to the OS.
+        openGallery.setOnClickListener(v-> startCropActivity());
+
+        uploadButton.setOnClickListener(v-> {
+            try {
+                String imagePath = getPathFromUri(this, Uri.parse(imageUri)); //Gets the real path of the file
+                Log.d("URI Real Path", imagePath);
+
+                saveToGallery(imagePath);
+
+                //Async Await
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        summarizeButton.setOnClickListener(v->{
+            Intent intent = new Intent(this, TextSummarization.class); // Switching to a new activity
+            //intent.putExtra("ImageName", "im2.jpg");
+            startActivity(intent);
+        });
+
+    }
+
+    private void saveToGallery(String imagePath) throws IOException {
+
+        //Input : imagePath : the real path of the image file
+
+        File f = new File(imagePath); // Retrieves the file from the directory
+        String uuid = UUID.randomUUID().toString();
+        //Amplify code to upload image to S3
+        Amplify.Storage.uploadFile(
+                Amplify.Auth.getCurrentUser().getUserId()+"/" + "im2.jpg",
+                f,
+                result -> Log.i("MyAmplifyApp", "Successfully uploaded: " + result.getKey()),
+                storageFailure -> Log.e("MyAmplifyApp", "Upload failed", storageFailure)
+        );
+    }
+
+    // Image Cropper Activity
+    private void startCropActivity() {
+        // Function that begins the crop activity
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
+    }
+
+    // On Activity Result
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri(); // Result uri holds the cropped image
+                imageView.setImageURI(resultUri); // Setting imageView with the chosen cropped picture
+
+                Log.d("URI", resultUri.toString());
+
+                imageUri = resultUri.toString(); // Assigning global variable the uri converted as string
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Log.d("Crop Image Error ", error.toString());
+            }
+        }
+    }
+
+    // Converts URI to a real path
+    public static String getPathFromUri(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+}
